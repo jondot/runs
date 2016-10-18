bba19f70e5aa3a91dd3b380a73d77a74bd3df45a
Dotan Nahum <jondotan@gmail.com>
2016-10-18 21:40:18 +0300

    use unit64

--- runs.go
+++ runs.go
@@ -6,7 +6,7 @@ import (
 
 type byMapped struct {
 	arr    []interface{}
-	mapper func(interface{}) int64
+	mapper func(interface{}) uint64
 }
 
 func (a byMapped) Len() int           { return len(a.arr) }
@@ -14,19 +14,19 @@ func (a byMapped) Swap(i, j int)      { a.arr[i], a.arr[j] = a.arr[j], a.arr[i] 
 func (a byMapped) Less(i, j int) bool { return a.mapper(a.arr[i]) < a.mapper(a.arr[j]) }
 
 // Detect will go over a list of `things` and group them according to a distance function called `belongs`.
-// You will need to provide a mapper function, that maps a thing to an ordinal (int64) value.
+// You will need to provide a mapper function, that maps a thing to an ordinal (uint64) value.
 //
 // For example:
-//	 grouped := runs.Detect(things, func(thing interface{}) int64 {
+//	 grouped := runs.Detect(things, func(thing interface{}) uint64 {
 //	  	return thing.(os.FileInfo).Size()
-//	 }, func(a, b int64) bool {
+//	 }, func(a, b uint64) bool {
 //	  	return math.Abs(float64(a-b)) < 1000
 //	 })
-func Detect(things []interface{}, mapper func(interface{}) int64, belongs func(a, b int64) bool) map[int64][]interface{} {
+func Detect(things []interface{}, mapper func(interface{}) uint64, belongs func(a, b uint64) bool) map[uint64][]interface{} {
 	sort.Sort(byMapped{arr: things, mapper: mapper})
 	sentinel := mapper(things[0])
 	last := sentinel
-	clusters := map[int64][]interface{}{}
+	clusters := map[uint64][]interface{}{}
 	for _, rec := range things {
 		ordinal := mapper(rec)
 		if belongs(last, ordinal) == false {
@@ -73,7 +73,7 @@ type GroupWalker struct {
 // For example:
 //  printer := &GroupWalker{}
 //  printer.Walk(grouped, &GroupPrinter{})
-func (g *GroupWalker) Walk(grouped map[int64][]interface{}, visitor GroupVisitor) {
+func (g *GroupWalker) Walk(grouped map[uint64][]interface{}, visitor GroupVisitor) {
 	for _, group := range grouped {
 		visitor.VisitHeader(group[0])
 		groupSize := len(group)
